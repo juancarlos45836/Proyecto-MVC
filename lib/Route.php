df33e6af943f93cc03c34b7737ae9ac4b336c2fe
CharlyCodigo <juancarlos45836@gmail.com>
2024-04-04 17:56:16 -0600

    Finalizando con la configuracion de rutas y el uso de expresiones regulares para pasar parametrso por medio de la url

--- lib/Route.php
+++ lib/Route.php
@@ -2,15 +2,53 @@
 namespace Lib;
 
 class Route{ 
-   public static  $routes = [];
+   private static  $routes = [];
 
-   static function get($url, $callaback){
-    self::$routes['GET'][$url] = $callaback;
+   public static function get($uri, $callaback){
+      // Metodo para quitar las diagonales externas de una uri
+      $uri = trim($uri,'/');
+      self::$routes['GET'][$uri] = $callaback;
 
    }
-   static function post($url, $callaback){  
-    self::$routes['POST'][$url] = $callaback;
+   public static function post($uri, $callaback){  
+    self::$routes['POST'][$uri] = $callaback;
    }
 
+   public static function dispatch(){
+       
+      $uri = $_SERVER['REQUEST_URI'];
+      $uri = trim($uri,'/');
+
+      //Metodo que sirve para saber porque metodo se esta enviado los datos del formulario
+      $method = $_SERVER['REQUEST_METHOD'];
    
+      //Iteracion que busca las uris y ejecuta las callaback que son mandadas por parametro
+      foreach (self::$routes[$method] as $route => $callback) {
+         //Comparando las 2 uris
+         // if($route == $uri){
+         //    $callback();
+         //    return;
+         // }
+         
+         //cahcar todo lo que esta despues de ":" que sea texto
+         if(strpos($route,':') !== false){
+            $route = preg_replace('#:[a-zA-Z]+#', '([a-zA-Z]+)', $route);
+         }
+         if(preg_match("#^$route$#",$uri,$matches)){
+            $params = array_slice($matches,1);
+
+            $response = $callback(...$params);
+            if(is_array($response)|| is_object($response)){
+               echo json_encode($response);
+            }else{
+               echo $response;
+            }
+            return;
+         }
+
+         
+        
+      }
+      echo 404;
+   }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,15 @@
 use Lib\Route;
 
 Route::get('/', function () {
-    echo'Ruta principal';
+    return [
+        'nombre' => 'apellidos',
+        'apellidos' => 'apellidos'
+    ];
 });
+
+Route::get('/login/:slug/:curso', function ($slug, $curso) {
+    return 'el slug es: '.$slug .'el curso es :  '.$curso;
+});
+
+
+Route::dispatch();

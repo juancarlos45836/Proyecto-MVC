16866c1fcafaef0ec31a2173ff3673a6cd6d4a47
CharlyCodigo <juancarlos45836@gmail.com>
2024-04-08 17:43:18 -0600

    feat: validacion de la entrada de email y contraseña de login

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -1,13 +1,28 @@
 <?php 
 namespace App\Controllers;
+use App\Models\User;
 
 class LoginController extends Controller{
     public function index(){
-        return $this->view('login',[]);
+        return $this->view('login',['emailIncorrect'=>'']);
     }  
     public function login(){
-      
-        return $this->view('dashboard',[]);
+        
+        $data = $_POST;
+        $users = new User();
+        $user = $users->where('email', $data['email'])->first();
+        if($user == null){
+            return $this->view('login',['emailIncorrect' => 'Este email no esta registrado']);
+        }else{
+            if($user['contrasena'] == $data['password']){
+                header('Location: /dashboard');
+            }else{
+                return $this->view('login',[
+                    'emailIncorrect'=>'',
+                    'passwordIncorrect'=>'La contraseña es incorrecta']);
+            }
+        }
+        
 
     }
 }

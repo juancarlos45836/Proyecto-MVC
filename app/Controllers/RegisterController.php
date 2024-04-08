e1345142e11357e9b6bb37884f4c84d2b9ee10cd
CharlyCodigo <juancarlos45836@gmail.com>
2024-04-08 17:39:42 -0600

    feat: logica para la insercion de un nuevo usuario por medio de la view register

--- app/Controllers/RegisterController.php
+++ app/Controllers/RegisterController.php
@@ -1,9 +1,16 @@
 <?php 
 namespace App\Controllers;
+use App\Models\User;
 
 class RegisterController extends Controller{
     public function index(){
     
         return $this->view('register',[]);
     }  
+    public function newUser(){
+        $data = $_POST;
+        $user = new User();
+        $user->create($data);
+        return $this->view('dashboard',[]);
+    }
 }
